8d5ae751bbe1b16433d835225486ea584e2b1932
arian <arian@aryweb.nl>
2010-03-24 21:14:07 +0100

    add addObservers and removeObserver methods

--- Source/moo-notifications.js
+++ Source/moo-notifications.js
@@ -1,5 +1,6 @@
 
 var NotificationCenter = {
+	
 	observers: {
 		'*': []
 	},
@@ -8,6 +9,17 @@ var NotificationCenter = {
 		notification = notification || '*';
 		if (!this.observers[notification]) this.observers[notification] = []; 
 		this.observers[notification].push(callback);
+	},
+	
+	addObservers: function(observers){
+		for(observer in observers) this.addObserver(observer,observers[observer]);
+	},
+	
+	removeObserver: function(notification,callback){
+		if (!this.observers[notification]) return;
+		for(var i =0;this.observers[notification].length;i++) {
+			if(this.observers[notification][i] === callback) delete this.observers[notification][i]; 
+		}
 	},
 
 	postNotification: function(notification, message) {
