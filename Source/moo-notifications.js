5c7c3579fddb5569e27cab29bbd432b91fe874c9
arian <arian@aryweb.nl>
2010-03-24 21:02:17 +0100

    Cleanup addObserver method

--- Source/moo-notifications.js
+++ Source/moo-notifications.js
@@ -5,19 +5,17 @@ var NotificationCenter = {
 	},
 
 	addObserver: function(notification, callback) {
-		if (this.observers[notification]) {
-			this.observers[notification].push(callback);
-		} else {
-			this.observers[notification] = [callback];
-		}
-		if (notification != '*') this.observers['*'].push(callback);
+		notification = notification || '*';
+		if (!this.observers[notification]) this.observers[notification] = []; 
+		this.observers[notification].push(callback);
 	},
 
 	postNotification: function(notification, message) {
 		if (this.observers[notification] || notification === '*') {
 			this.observers[notification].each(function(observer) {
 				observer(message);
-		});
+			});
+		};
 	}
 
 };
